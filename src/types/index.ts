fc1fc78f193ee6c8ea4c9b3b2fbceb0e630eca38
rohangahlot27 <rohan.gahlot27@gmail.com>
2025-07-08 06:49:45 +0530

    Add files via upload

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type RowData = {
+  id: string;
+  jobRequest: string;
+  submitted: string;
+  status: string;
+  submitter: string;
+  url: string;
+  assigned: string;
+  priority: string;
+  dueDate: string;
+  value: string;
+};
+
